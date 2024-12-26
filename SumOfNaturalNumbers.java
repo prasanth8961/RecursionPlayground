0bec020998442457d9f8f6fcd0a903dd28976470
prasanth8961 <prasanthprs143@gmail.com>
2024-12-26 15:41:26 +0530

    finding sum of N natural numbers

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class SumOfNaturalNumbers {
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
